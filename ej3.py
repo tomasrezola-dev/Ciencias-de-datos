4972e5d0b89e8fee20ea54594eee7acefad99c3f
unknown <tomasrezola@gmail.com>
2026-08-17 20:16:27 -0300

    dejar master con README

--- ej3.py
+++
@@ -1,5 +0,0 @@
-from math import pi
-def areaC (radio):
-    return pi * (radio**2)
-
-print(areaC(5))
--- ej4.py
+++
@@ -1,3 +0,0 @@
-frutas = ['mandarina', 'manzana', 'banana', 'higo', 'macaco']
-for fruta in frutas:
-    print(f"Fruta {fruta}")
